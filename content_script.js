27fdad20044244ed0720ef9fcf13b7d1d59baee2
jbryant32 <jason.bryant28@gmail.com>
2017-12-22 22:06:54 -0800

    INIT

---
+++ content_script.js
@@ -0,0 +1,26 @@
+﻿var match = /^\d+$/;
+
+setTimeout(() => {
+    var ad = Array.from(document.getElementsByClassName("mgbox"));
+    var allDiv = Array.from(document.getElementsByTagName("div"));
+    
+    allDiv.forEach((ele) => {
+        
+        var div = ele.getAttribute("id");
+        if (div !== null)
+        {
+            if (div.match(match))
+            {
+                
+                ele.remove();
+            }
+            
+        }
+    });
+
+
+    ad.forEach((ele) => {
+        ele.remove();
+    });
+}, 4000);
+
